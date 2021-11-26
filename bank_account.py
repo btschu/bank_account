a732de14d67e7c68e3b483478e51a668eb168a97
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-11-25 22:43:30 -0700

    finished assignment

--- bank_account.py
+++ bank_account.py
@@ -1,13 +1,34 @@
 class BankAccount:
-    # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate, balance): 
-        # your code here! (remember, instance attributes go here)
-        # don't worry about user info here; we'll involve the User class soon
+    all_accounts = []
+    def __init__(self, int_rate = .015, balance = 0):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.all_accounts.append(self)
     def deposit(self, amount):
-        # your code here
-    def withdraw(self, amount):
-        # your code here
+        self.balance += amount
+        return self
+    def withdrawal(self, amount):
+        if amount > self.balance:
+            self.balance = (self.balance - amount) - 5
+            print("Insufficient Funds: Charging a $5 fee")
+        else:
+            self.balance -= amount
+        return self
     def display_account_info(self):
-        # your code here
+        print(f"Account Balance: {self.balance}")
+        return self
     def yield_interest(self):
-        # your code here
+        self.balance = self.balance * (1 + self.int_rate)
+        return self
+
+    @classmethod
+    def all_balances(cls):
+        sum = 0
+        for account in cls.all_accounts:
+            print("Account Balance:",account.balance,"Interest Rate:",account.int_rate)
+        return sum
+
+member_one = BankAccount().deposit(500).deposit(50).deposit(75).withdrawal(100).yield_interest().display_account_info()
+member_two = BankAccount().deposit(400).deposit(600).withdrawal(100).withdrawal(200).withdrawal(600).withdrawal(101).yield_interest().display_account_info()
+
+BankAccount.all_balances()
