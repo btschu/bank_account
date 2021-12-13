ba4be35a58634ab70d202f74f240c63c45ba208d
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-13 15:29:41 -0700

    Added dollar signs

--- bank_account.py
+++ bank_account.py
@@ -18,7 +18,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Account Balance: {self.balance}")
+        print(f"Account Balance: ${self.balance}")
         return self
 
     def yield_interest(self):
@@ -29,11 +29,11 @@ class BankAccount:
     def all_balances(cls):
         sum = 0
         for account in cls.all_accounts:
-            print("Account Balance:",account.balance,"Interest Rate:",account.int_rate)
+            print(f"Account Balance: ${account.balance} Interest Rate: {account.int_rate}")
         return sum
 
-member_one = BankAccount().deposit(500).deposit(50).deposit(75).withdrawal(100).yield_interest().display_account_info()
-member_two = BankAccount().deposit(400).deposit(600).withdrawal(100).withdrawal(200).withdrawal(600).withdrawal(101).yield_interest().display_account_info()
+savings_account = BankAccount().deposit(500).deposit(50).deposit(75).withdrawal(100).yield_interest()
+checking_account = BankAccount().deposit(400).deposit(600).withdrawal(100).withdrawal(200).withdrawal(600).withdrawal(101).yield_interest()
 
 BankAccount.all_balances()
 
