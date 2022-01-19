cd62a6776b169fc0141c51d4126d98ce26566274
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-01-18 17:18:32 -0700

    fixed "create 2 accounts"

--- bank_account.py
+++ bank_account.py
@@ -10,11 +10,11 @@ class BankAccount:
         return self
 
     def withdrawal(self, amount):
-        if amount > self.balance:
-            self.balance = (self.balance - amount) - 5
-            print("Insufficient Funds: Charging a $5 fee")
-        else:
+        if(self.balance - amount) >= 0:
             self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
         return self
 
     def display_account_info(self):
@@ -22,22 +22,23 @@ class BankAccount:
         return self
 
     def yield_interest(self):
-        self.balance = self.balance * (1 + self.int_rate)
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
         return self
 
     @classmethod
     def all_balances(cls):
-        sum = 0
         for account in cls.all_accounts:
-            print(f"Account Balance: ${account.balance} Interest Rate: {account.int_rate}")
-        return sum
+            account.display_account_info()
 
-savings_account = BankAccount().deposit(500).deposit(50).deposit(75).withdrawal(100).yield_interest()
-checking_account = BankAccount().deposit(400).deposit(600).withdrawal(100).withdrawal(200).withdrawal(600).withdrawal(101).yield_interest()
+
+savings_account = BankAccount(.04,1500)
+checking_account = BankAccount(.02,4500)
+
+savings_account.deposit(500).deposit(50).deposit(76).withdrawal(100).yield_interest()
+checking_account.deposit(400).deposit(600).withdrawal(100).withdrawal(200).withdrawal(600).withdrawal(101).yield_interest()
 
 BankAccount.all_balances()
-
-
 
 #! Instructor's solution
 
